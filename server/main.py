772f97dbab6c8ee29dfe8f086cd68b43dc9eb3d4
Daniele Maccioni <gendoikari@develer.com>
2017-10-25 12:05:51 +0200

    database table

--- server/main.py
+++ server/main.py
@@ -1,22 +1,36 @@
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 import time
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chat.db'
+db = SQLAlchemy(app)
 
-STORE_MESSAGES = []
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    sender = db.Column(db.String, nullable=False)
+    content = db.Column(db.String, nullable=False)
+    timestamp = db.Column(db.Integer, nullable=False)
+
+    def json(self):
+        return {'id': self.id, 'sender': self.sender, 'content': self.content, 'timestamp': self.timestamp}
+
+db.create_all()
 
 @app.route('/v1/messages', methods=['GET'])
 def messages():
-    return jsonify({'messages': STORE_MESSAGES})
+    messages = [m.json() for m in Message.query.all()]
+    return jsonify({'messages': messages})
 
 @app.route('/v1/messages', methods=['POST'])
 def create_message():
     json = request.get_json()
-    new_message = {
-        'id': len(STORE_MESSAGES),
-        'sender': json['sender'],
-        'content': json['content'],
-        'timestamp': time.time(),
-    }
-    STORE_MESSAGES.append(new_message)
-    return jsonify(new_message)
+    new_message = Message(
+        sender=json['sender'],
+        content=json['content'],
+        timestamp=time.time(),
+    )
+    db.session.add(new_message)
+    db.session.commit()
+    return jsonify(new_message.json())
